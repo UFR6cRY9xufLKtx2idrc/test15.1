f9d6993c94e355faddf3ab90a95575ca26691684
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2023-02-05 16:20:09 +0000

    Update collector.go

--- plugin/executable/metrics_collector/collector.go
+++ plugin/executable/metrics_collector/collector.go
@@ -22,8 +22,8 @@ package metrics_collector
 import (
 	"context"
 	"errors"
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/query_context"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/plugin/executable/sequence"
 	"github.com/prometheus/client_golang/prometheus"
 	"time"
 )
