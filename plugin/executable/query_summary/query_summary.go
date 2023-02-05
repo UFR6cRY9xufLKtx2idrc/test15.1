7192c5b9ca56bd773fa0836ca83105d96363f20e
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2023-02-05 16:21:47 +0000

    Update query_summary.go

--- plugin/executable/query_summary/query_summary.go
+++ plugin/executable/query_summary/query_summary.go
@@ -21,8 +21,8 @@ package query_summary
 
 import (
 	"context"
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/query_context"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/plugin/executable/sequence"
 	"go.uber.org/zap"
 )
 
