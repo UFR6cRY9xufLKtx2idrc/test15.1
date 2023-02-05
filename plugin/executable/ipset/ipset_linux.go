573a823258ac62539233c66a9f2f6a3081bf41dd
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2023-02-05 16:19:06 +0000

    Update ipset_linux.go

--- plugin/executable/ipset/ipset_linux.go
+++ plugin/executable/ipset/ipset_linux.go
@@ -24,7 +24,7 @@ package ipset
 import (
 	"context"
 	"fmt"
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/query_context"
 	"github.com/miekg/dns"
 	"github.com/nadoo/ipset"
 	"net/netip"
