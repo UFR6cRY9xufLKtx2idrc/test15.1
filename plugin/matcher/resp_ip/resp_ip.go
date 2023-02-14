ea9655e8ea05c22c060da90e5802aff354b44d5e
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2023-02-14 12:54:03 +0000

    Update resp_ip.go

--- plugin/matcher/resp_ip/resp_ip.go
+++ plugin/matcher/resp_ip/resp_ip.go
@@ -20,46 +20,46 @@
 package resp_ip
 
 import (
-	"github.com/IrineSistiana/mosdns/v5/pkg/matcher/netlist"
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
-	"github.com/IrineSistiana/mosdns/v5/plugin/matcher/base_ip"
-	"github.com/miekg/dns"
-	"net"
-	"net/netip"
+    "github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/matcher/netlist"
+    "github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/query_context"
+    "github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/plugin/executable/sequence"
+    "github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/plugin/matcher/base_ip"
+    "github.com/miekg/dns"
+    "net"
+    "net/netip"
 )
 
 const PluginType = "resp_ip"
 
 func init() {
-	sequence.MustRegMatchQuickSetup(PluginType, QuickSetup)
+    sequence.MustRegMatchQuickSetup(PluginType, QuickSetup)
 }
 
 type Args = base_ip.Args
 
 func QuickSetup(bq sequence.BQ, s string) (sequence.Matcher, error) {
-	return base_ip.NewMatcher(bq, base_ip.ParseQuickSetupArgs(s), matchRespAddr)
+    return base_ip.NewMatcher(bq, base_ip.ParseQuickSetupArgs(s), matchRespAddr)
 }
 
 func matchRespAddr(qCtx *query_context.Context, m netlist.Matcher) (bool, error) {
-	r := qCtx.R()
-	if r == nil {
-		return false, nil
-	}
-	for _, rr := range r.Answer {
-		var ip net.IP
-		switch rr := rr.(type) {
-		case *dns.A:
-			ip = rr.A
-		case *dns.AAAA:
-			ip = rr.AAAA
-		default:
-			continue
-		}
-		addr, ok := netip.AddrFromSlice(ip)
-		if ok && m.Match(addr) {
-			return true, nil
-		}
-	}
-	return false, nil
+    r := qCtx.R()
+    if r == nil {
+        return false, nil
+    }
+    for _, rr := range r.Answer {
+        var ip net.IP
+        switch rr := rr.(type) {
+        case *dns.A:
+            ip = rr.A
+        case *dns.AAAA:
+            ip = rr.AAAA
+        default:
+            continue
+        }
+        addr, ok := netip.AddrFromSlice(ip)
+        if ok && m.Match(addr) {
+            return true, nil
+        }
+    }
+    return false, nil
 }
