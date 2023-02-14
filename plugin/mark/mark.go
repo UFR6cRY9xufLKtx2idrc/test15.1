0350a9c4b9b05ef368a8251d0d9763fd641e5d32
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2023-02-14 12:42:48 +0000

    Update mark.go

--- plugin/mark/mark.go
+++ plugin/mark/mark.go
@@ -21,8 +21,8 @@ package mark
 
 import (
 	"context"
-	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/pkg/query_context"
+	"github.com/UFR6cRY9xufLKtx2idrc/mosdns/main/plugin/executable/sequence"
 	"strconv"
 	"strings"
 )
